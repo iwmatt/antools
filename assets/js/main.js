ba6fedb30d698ff9fa8f1f1f49c976bcb22ecac8
iwnl-matt <orshan1003@gmail.com>
2023-01-23 12:07:02 +0300

    added swiper, jquery, and new blocks

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(() => {
+
+    if (Swiper && $('.team__swiper')) {
+        const teamSwiper = new Swiper('.team__swiper',{
+            slidesPerView: 1,
+            pagination: {
+                el: '.team__swiper-pagination'
+            },
+            allowTouchMove: true,
+            spaceBetween: 2000,
+            speed: 700,
+        })
+        const [teamSwiperLeft, teamSwiperRight] = [
+            $('.team__swiper-arrow-left'),
+            $('.team__swiper-arrow-right'),
+        ]
+        teamSwiperLeft.click(() => {
+            teamSwiper.slidePrev()
+        })
+        teamSwiperRight.click(() => {
+            console.log("right")
+            teamSwiper.slideNext()
+        })
+    } else {
+        console.log('swiper.js .team__swiper error')
+    }
+    
+})
